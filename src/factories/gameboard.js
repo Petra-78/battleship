808f42248fee2f9b6013a4060108fe5297772423
Petra-78 <partos.petra.0305@gmail.com>
2025-09-04 17:49:06 +0200

    finish gameboard + tests (placeShip safety checks and recieveAttack method)

--- src/factories/gameboard.js
+++ src/factories/gameboard.js
@@ -4,6 +4,7 @@ class Gameboard {
   constructor() {
     this.board = [];
     this.ships = [];
+    this.missedAttacks = [];
     for (let i = 0; i < 10; i++) {
       const rows = [];
       for (let j = 0; j < 10; j++) {
@@ -13,15 +14,58 @@ class Gameboard {
     }
   }
 
-  placeShip(length, coordinates) {
+  placeShip(length, coordinates, direction) {
     const ship = new Ship(length);
     this.ships.push(ship);
     let x = coordinates[0];
     let y = coordinates[1];
-    for (let i = 0; i < length; i++) {
-      this.board[x + i][y] = ship;
+    if (direction === 'vertical' && x + length <= 10 && x >= 0) {
+      for (let i = 0; i < length; i++) {
+        if (this.board[x + i][y] !== 0) {
+          throw new Error('that spot is already taken');
+        } else {
+          this.board[x + i][y] = ship;
+        }
+      }
+      return ship;
+    } else if (direction === 'horizontal' && y + length <= 10 && y >= 0) {
+      for (let i = 0; i < length; i++) {
+        if (this.board[x][y + i] !== 0) {
+          throw new Error('this spot is already taken');
+        } else {
+          this.board[x][y + i] = ship;
+        }
+      }
+      return ship;
+    } else return false;
+  }
+
+  receiveAttack(x, y) {
+    if (x < 0 || x > 10 || y < 0 || y > 10) {
+      throw new Error('invalid coordinates');
     }
-    return ship;
+    if (this.board[x][y] !== 0) {
+      const ship = this.board[x][y];
+      ship.hit();
+      const result = this.isALlSunk();
+      return result || ship;
+    } else {
+      this.missedAttacks.push([x, y]);
+      return [x, y];
+    }
+  }
+
+  isALlSunk() {
+    let sunkCount = 0;
+    for (const ship of this.ships) {
+      if (ship.length === ship.hits) {
+        ship.sunk = true;
+        sunkCount++;
+      }
+    }
+    if (sunkCount === this.ships.length) {
+      return true;
+    }
   }
 }
 
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -16,9 +16,9 @@ test('make a 10x10 grid', () => {
   ]);
 });
 
-test('places a ship on the board', () => {
+test('places a ship on the board vertically', () => {
   const board = new Gameboard();
-  const ship = board.placeShip(3, [0, 1]);
+  const ship = board.placeShip(3, [0, 1], 'vertical');
   expect(board.board[0][1]).toBe(ship);
   expect(board.board[1][1]).toBe(ship);
   expect(board.board[2][1]).toBe(ship);
@@ -26,4 +26,63 @@ test('places a ship on the board', () => {
   expect(ship.length).toBe(3);
   expect(ship.hits).toBe(0);
   expect(ship.sunk).toBe(false);
+});
+
+test('places a ship on the board horizontally', () => {
+  const board = new Gameboard();
+  const ship = board.placeShip(3, [0, 1], 'horizontal');
+  expect(board.board[0][1]).toBe(ship);
+  expect(board.board[0][2]).toBe(ship);
+  expect(board.board[0][3]).toBe(ship);
+
+  expect(ship.length).toBe(3);
+  expect(ship.hits).toBe(0);
+  expect(ship.sunk).toBe(false);
+});
+
+test('throws an error when overlapped', () => {
+  const board = new Gameboard();
+  board.placeShip(3, [0, 1], 'horizontal');
+  expect(() => {
+    board.placeShip(3, [0, 1], 'vertical');
+  }).toThrow('that spot is already taken');
+});
+
+test('returns false when ship placed out of board', () => {
+  const board = new Gameboard();
+  const ship = board.placeShip(3, [0, 9], 'horizontal');
+  expect(ship).toBe(false);
+});
+
+test('ships array contains the placed ships', () => {
+  const board = new Gameboard();
+  const ship1 = board.placeShip(3, [0, 0], 'horizontal');
+  const ship2 = board.placeShip(2, [1, 2], 'horizontal');
+  const ship3 = board.placeShip(5, [5, 0], 'vertical');
+  expect(board.ships[0]).toBe(ship1);
+  expect(board.ships[1]).toBe(ship2);
+  expect(board.ships[2]).toBe(ship3);
+});
+
+test('sends hit to a ship', () => {
+  const board = new Gameboard();
+  const ship = board.placeShip(3, [0, 0], 'horizontal');
+  const attack = board.receiveAttack(0, 0);
+  expect(attack).toBe(ship);
+});
+
+test('returns the coordinates if the hit missed', () => {
+  const board = new Gameboard();
+  const ship = board.placeShip(3, [0, 0], 'vertical');
+  const attack = board.receiveAttack(0, 1);
+  expect(attack).toEqual([0, 1]);
+});
+
+test('notify if all ships have been sunk', () => {
+  const board = new Gameboard();
+  const ship = board.placeShip(3, [0, 0], 'horizontal');
+  board.receiveAttack(0, 0);
+  board.receiveAttack(0, 1);
+  const attack3 = board.receiveAttack(0, 2);
+  expect(attack3).toBe(true);
 });
