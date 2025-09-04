dcaae11cb797e95dd7f00032c5609dc618a11bbd
Petra-78 <partos.petra.0305@gmail.com>
2025-09-04 15:36:38 +0200

    add placeShip method + tests

--- src/factories/gameboard.js
+++ src/factories/gameboard.js
@@ -1,15 +1,27 @@
+import { Ship } from './ship';
+
 class Gameboard {
   constructor() {
-    const board = [];
-    const cells = 0;
+    this.board = [];
+    this.ships = [];
     for (let i = 0; i < 10; i++) {
       const rows = [];
       for (let j = 0; j < 10; j++) {
-        rows.push(cells);
+        rows.push(0);
       }
-      board.push(rows);
+      this.board.push(rows);
     }
-    return board;
+  }
+
+  placeShip(length, coordinates) {
+    const ship = new Ship(length);
+    this.ships.push(ship);
+    let x = coordinates[0];
+    let y = coordinates[1];
+    for (let i = 0; i < length; i++) {
+      this.board[x + i][y] = ship;
+    }
+    return ship;
   }
 }
 
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -2,7 +2,7 @@ import { Gameboard } from '../factories/gameboard';
 
 test('make a 10x10 grid', () => {
   const board = new Gameboard();
-  expect(board).toEqual([
+  expect(board.board).toEqual([
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -14,4 +14,16 @@ test('make a 10x10 grid', () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+});
+
+test('places a ship on the board', () => {
+  const board = new Gameboard();
+  const ship = board.placeShip(3, [0, 1]);
+  expect(board.board[0][1]).toBe(ship);
+  expect(board.board[1][1]).toBe(ship);
+  expect(board.board[2][1]).toBe(ship);
+
+  expect(ship.length).toBe(3);
+  expect(ship.hits).toBe(0);
+  expect(ship.sunk).toBe(false);
 });
