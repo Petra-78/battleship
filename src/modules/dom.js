5fa62851190003cd2ae357d989eeb54ce512ddc5
Petra-78 <partos.petra.0305@gmail.com>
2025-09-09 14:05:05 +0200

    add board rendering and game initializing

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -3,10 +3,14 @@ function renderBoards(player, grid) {
   const container = document.querySelector(`.${grid}`);
   container.innerHTML = '';
 
-  gameboard.forEach((row) => {
-    row.forEach((cell) => {
+  gameboard.forEach((row, y) => {
+    row.forEach((cell, x) => {
       const div = document.createElement('div');
       div.classList.add('cell');
+
+      div.dataset.x = x;
+      div.dataset.y = y;
+
       div.style.backgroundColor = 'var(--purple)';
 
       if (cell !== 0) {
--- src/modules/game.js
+++ src/modules/game.js
@@ -1,18 +1,37 @@
 import { Player } from '../factories/players';
 import { renderBoards } from './dom';
+import { Ship } from '../factories/ship';
 
-const player1 = new Player('human');
-const computer = new Player('computer');
+const initGame = function () {
+  const p1 = new Player('human');
+  const p2 = new Player('computer');
 
-player1.board.placeShip(2, [0, 1], 'horizontal');
-player1.board.placeShip(3, [6, 1], 'horizontal');
-player1.board.placeShip(3, [9, 3], 'vertical');
-player1.board.placeShip(4, [5, 5], 'vertical');
-player1.board.placeShip(5, [0, 9], 'horizontal');
+  p1.board.placeShip(new Ship(2), [0, 1], 'horizontal');
+  p1.board.placeShip(new Ship(3), [6, 1], 'horizontal');
+  p1.board.placeShip(new Ship(3), [9, 3], 'vertical');
+  p1.board.placeShip(new Ship(4), [5, 5], 'vertical');
+  p1.board.placeShip(new Ship(5), [0, 9], 'horizontal');
 
-computer.attack(player1);
+  renderBoards(p1, 'grid1');
+  renderBoards(p2, 'grid2');
 
-console.log(computer.board);
+  const computerContainer = document.querySelector('.grid2');
 
-renderBoards(player1, 'grid1');
-renderBoards(computer, 'grid2');
+  computerContainer.addEventListener('click', (e) => {
+    const cell = e.target;
+    if (!cell.classList.contains('cell')) return;
+
+    const x = parseInt(cell.dataset.x, 10);
+    const y = parseInt(cell.dataset.y, 10);
+
+    p1.attack(p2, [x, y]);
+
+    renderBoards(p2, 'grid2');
+    renderBoards(p1, 'grid1');
+
+    p2.attack(p1);
+    renderBoards(p1, 'grid1');
+  });
+};
+
+initGame();
