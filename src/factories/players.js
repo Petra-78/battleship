c9238c45e1565f996f65d6ead1e9c1755f1f1194
Petra-78 <partos.petra.0305@gmail.com>
2025-09-10 13:45:50 +0200

    add check for invalid attacks

--- src/factories/players.js
+++ src/factories/players.js
@@ -11,13 +11,38 @@ class Player {
 
   attack(player, coordinates) {
     if (this.type === 'human') {
-      const attack = player.board.receiveAttack(coordinates[0], coordinates[1]);
-      return attack;
+      const [x, y] = coordinates;
+      const cell = player.board.board[y][x];
+
+      const alreadyMissed = player.board.missedAttacks.some(
+        (coord) => coord[0] === x && coord[1] === y
+      );
+
+      if (alreadyMissed === true || cell.hit === true) {
+        alert('you already attacked there!');
+        return 'invalid';
+      }
+
+      return player.board.receiveAttack(x, y);
     } else if (this.type === 'computer') {
-      const generateX = Math.floor(Math.random() * 10);
-      const generateY = Math.floor(Math.random() * 10);
-      const attack = player.board.receiveAttack(generateX, generateY);
-      return attack;
+      let valid = false;
+      let x, y;
+
+      while (valid !== true) {
+        x = Math.floor(Math.random() * 10);
+        y = Math.floor(Math.random() * 10);
+        const cell = player.board.board[y][x];
+
+        const alreadyMissed = player.board.missedAttacks.some(
+          (coord) => coord[0] === x && coord[1] === y
+        );
+
+        if (alreadyMissed === false || cell === 0) {
+          valid = true;
+        }
+      }
+
+      return player.board.receiveAttack(x, y);
     }
   }
 }
